c0b1c1c21555fb49e0ecf0d32318f1aa599a3c9f
tigo3 <tigo3@users.noreply.github.com>
2025-04-06 11:13:26 +0000

    deploy: 5f7580dc3e4fcec74105a1480ed1b046733ab454

--- assets/ProjectsSection-cPM9hpFs.js
+++ assets/ProjectsSection-cPM9hpFs.js
@@ -1,4 +1,4 @@
-import{c as l,j as e}from"./index-Cm8sXCLE.js";import"./react-vendor-ByTr29QW.js";import"./firebase-vendor-CAk-8R_y.js";/**
+import{c as l,j as e}from"./index-BypZZ9U0.js";import"./react-vendor-ByTr29QW.js";import"./firebase-vendor-CAk-8R_y.js";/**
  * @license lucide-react v0.484.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -9,4 +9,4 @@ import{c as l,j as e}from"./index-Cm8sXCLE.js";import"./react-vendor-ByTr29QW.js
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
  */const x=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],m=l("external-link",x),y=({projects:r,title:o})=>e.jsxs("section",{className:"container mx-auto px-4 py-16 rounded-lg shadow-xl",children:[e.jsx("h2",{className:"text-3xl font-bold text-center mb-12",style:{color:"var(--title-color)"},children:o}),e.jsx("div",{className:"grid md:grid-cols-2 gap-8 max-w-4xl mx-auto",children:r.map((s,t)=>{const a=e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"flex justify-between items-start mb-4 ",children:[e.jsx(d,{className:"text-blue-400",size:24}),s.link&&e.jsx(m,{size:20,className:"text-gray-500 "})]}),e.jsx("h3",{className:"text-xl font-semibold mb-2",style:{color:"var(--h3title-color)"},children:s.title}),e.jsx("p",{className:"text-gray-400 text-text",children:s.description}),e.jsx("div",{className:"mt-4 flex flex-wrap gap-2",children:s.tags.map((n,c)=>e.jsx("span",{className:"px-3 py-1 rounded-full text-sm  bg-gray-800/50",children:n},c))})]});return s.link?e.jsx("a",{href:s.link,target:"_blank",rel:"noopener noreferrer",className:"block rounded-lg p-6 shadow-lg hover:bg-gray-700 transition-colors hover:shadow-xl sectionbg",children:a},t):e.jsx("div",{className:"rounded-lg p-6 shadow-lg sectionbg",children:a},t)})})]});export{y as default};
-//# sourceMappingURL=ProjectsSection-BMAKDWIF.js.map
+//# sourceMappingURL=ProjectsSection-cPM9hpFs.js.map
