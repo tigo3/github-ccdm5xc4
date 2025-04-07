0888cbd4d2c54d07a3cd62271a576d1adc1b97e7
tigo3 <tigo3@users.noreply.github.com>
2025-04-07 18:22:06 +0000

    deploy: ca862d4a43497b8dbc1922954d8a03bbc75f9350

--- assets/ProjectsSection-Du7FsPtc.js
+++ assets/ProjectsSection-Du7FsPtc.js
@@ -1,4 +1,4 @@
-import{c as n,j as e}from"./index-D0PJAnkW.js";import"./react-vendor-ByTr29QW.js";import"./firebase-vendor-CJFYC82U.js";/**
+import{c as n,j as e}from"./index-D7NIX2iE.js";import"./react-vendor-ByTr29QW.js";import"./firebase-vendor-CJFYC82U.js";/**
  * @license lucide-react v0.484.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -9,4 +9,4 @@ import{c as n,j as e}from"./index-D0PJAnkW.js";import"./react-vendor-ByTr29QW.js
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
  */const d=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],m=n("external-link",d),b=({projects:r,title:l})=>e.jsxs("section",{className:"container mx-auto px-4 py-16 shadow-xl",children:[e.jsx("h2",{className:"text-3xl font-bold text-center mb-12 text-title",children:l}),e.jsx("div",{className:"grid md:grid-cols-2 gap-8 max-w-4xl mx-auto",children:r.map((t,s)=>{const a=e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"flex justify-between items-start mb-4 ",children:[e.jsx(x,{className:"text-primary",size:24}),t.link&&e.jsx(m,{size:20,className:"text-gray-500 "})]}),e.jsx("h3",{className:"text-xl font-semibold mb-2 text-h3title",children:t.title}),e.jsx("p",{className:"text-text",children:t.description}),e.jsx("div",{className:"mt-4 flex flex-wrap gap-2",children:t.tags.map((o,i)=>e.jsx("span",{className:"px-3 py-1 rounded-full text-sm bg-gradient-to-br from-background to-background-secondary text-text",children:o},i))})]});return t.link?e.jsx("a",{href:t.link,target:"_blank",rel:"noopener noreferrer",className:"block  p-6 shadow-lg hover:bg-gray-700 transition-colors hover:shadow-xl bg-section",children:a},s):e.jsx("div",{className:" p-6 shadow-lg bg-section",children:a},s)})})]});export{b as default};
-//# sourceMappingURL=ProjectsSection-BBhTc9Zc.js.map
+//# sourceMappingURL=ProjectsSection-Du7FsPtc.js.map
